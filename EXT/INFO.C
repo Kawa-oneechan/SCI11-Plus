fa49a982f7e26d83ae7c6f5bc48cd7e8305c6aad
Kawa <kawaoneechan@gmail.com>
2020-08-18 17:07:01 +0200

    Update info.c for release

--- EXT/INFO.C
+++ EXT/INFO.C
@@ -1,6 +1,6 @@
 char makeDate[] = __DATE__;
 char makeTime[] = __TIME__;
 char maker[] = "Kawa";
-char makeLocation[] = "c:/interp";
-char version[] = "1.KAWA.001";
+char makeLocation[] = "C:/EXT";
+char version[] = "1.KAWA.042";
 char makeComment[] = "Built by Kawa";
