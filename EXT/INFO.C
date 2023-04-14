d6d168e6766fe7f742e481565e6bca159ff45b99
Kawa <kawaoneechan@gmail.com>
2023-04-14 16:16:31 +0200

    Bump version for release

--- EXT/INFO.C
+++ EXT/INFO.C
@@ -2,5 +2,5 @@ char makeDate[] = __DATE__;
 char makeTime[] = __TIME__;
 char maker[] = "Kawa";
 char makeLocation[] = "C:/EXT";
-char version[] = "1.KAWA.043";
+char version[] = "1.KAWA.044";
 char makeComment[] = "Built by Kawa";
